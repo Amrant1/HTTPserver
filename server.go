5176c73ca1860d47bd723a7b17af76cfa0ddb661
Amrant1 <165089883+Amrant1@users.noreply.github.com>
2025-10-03 09:32:38 -0700

    init

--- server.go
+++ server.go
@@ -1,27 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"errors"
 	"net/http"
 )
 
-
-type Server struct {
-	Addr  string// Address to listen on
-}
-
-
-func ListenAndServe(s *Server) error {
-	err := errors.New("404 Not Found")
-	return err
-}
-
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "404 Not Found")
-	})
-	http.ListenAndServe(":8080", mux)
+	ser := &http.Server{
+		Addr:":8080",
+		Handler: mux,
+	}
+	if err := ser.ListenAndServe(); err != nil {
+		ser.Close()
+	}
+	
 }
 
